5874a37e0c3b297e8fd1b970f4afa6e3df7d65cc
Bo Chen <bchen16@u.rochester.edu>
2017-11-21 17:59:01 -0500

    modified:   getData.py

--- getData.py
+++ getData.py
@@ -1,20 +1,33 @@
 import sys
 
+
+
+
 with open('AMiner-Author.txt','r') as f:
 	lines = list(f.readlines())
-
-University = []
-
+d = {}
+key = ['univ','inc','corp','scho','lab','service','research','inst']
 for i in lines:
 	if len(i) > 10:
 		if i[1] == 'a':
 			raw = i[3:]
-			raw = raw.split(',')
+			raw = raw.split(';')
 			for j in raw:
-				University.append(j)
-for i in range(0,100):
-    print(University[i])
+				temp = j.split(',')
+				for k in range(0,len(temp)):
+					t = ''
+					for l in key:
+						if l in temp[k].lower():
+							t = t + ',' + temp[k]
+						else:
+							d[t] = temp[k+1:]
 
-f1 = open('affiliations.txt','w')
-for i in University:
-	f1.write("%s\n" % i)
+print(len(d))
+#for i,j in d.items():
+#	print(i)
+#	print(j)
+	
+
+
+
+		
