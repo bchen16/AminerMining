ee0c0a6d6444f3dc1f3b2fb9f60ba6aa7953c6a6
Bo Chen <bchen16@u.rochester.edu>
2017-11-25 21:26:12 -0500

    make data + get data

--- makeData.py
+++ makeData.py
@@ -9,9 +9,14 @@ with open('AMiner-Author.txt','r') as f:
         Alines = list(f.readlines())
 print('Complete!')
 
+
+print()
+print('Creating raw author dictionary at No.')
 #Author dictionary raw data from Aminer-Paper.txt
-Author_RawUniversity = {}
+Author_University_Raw = {}
 #Usable Author dictionary, with author name/affiliation-year information
+    #key:('string' name.'int' times appeared)
+    #affiliation:[('string' university raw name,'int' year)]
 Author_Univ_Year={}
 
 #Loading author information from Aminer-Author.txt, omitting those without affiliation
@@ -23,61 +28,33 @@ while(i < len(Alines)):
         if Alines[i][1] == 'i' and Alines[i][2] == 'n' and Alines[i][3] == 'd' and Alines[i][4] == 'e' and Alines[i][5] == 'x':
             key = Alines[i+1][3:]
             affiliation = Alines[i+2][3:]
-            if len(affiliation)>8:
-                Author_RawUniversity[key] = affiliation
+            if len(affiliation)>3:
+                Author_University_Raw[key] = affiliation
             i = i + 9       
         else:
             i = i + 1
     else:
         i = i + 1
-    if i%1000000 == 0:
-        print(len(Author_RawUniversity))
+    if i%2500000 == 0:
+        print(len(Author_University_Raw))
 
-print('Total author loaded with affiliation(s):' + str(len(Author_RawUniversity)))
-
+print('Total author loaded with affiliation(s):' + str(len(Author_University_Raw)))
 
 #Loading author information from Aminer-Paper.txt, pairing with chronical information
-multi_author=[]
-multi_univ=[]
-multi_author_del=[]
+i = 0
+while(i+3 < len(lines)):
+    key = ''
+    currU = ''
+    affiliation = []
+    if len(lines[i])<6:
+        i = i + 1
+    elif lines[i][1] == 'i' and lines[i][2] == 'n' and lines[i][3] == 'd' and lines[i][4] == 'e' and lines[i][5] == 'x':
+        key = lines[i+2][3:]
+        currU = lines[i+3][3:]
+        i = i + 5
+    else:
+        i = i + 1 
+    if i%2500000 == 0:
+        print(i)
+print(i)
 
-for i in range(len(lines)):
-    if len(lines[i]) > 4:
-        if lines[i][1] == '@' and lines[i+1][3]!='-':
-            author_key = lines[i][3:-1]
-            univ_key=lines[i+1][3:-1]
-            
-            if ';' in author_key:
-                multi_author_del.append(author_key)
-            
-            Author_Univ_Year.setdefault(author_key,{}) 
-            if univ_key in Author_Univ_Year[author_key]:
-                Author_Univ_Year[author_key][univ_key].append(lines[i+2][3:-1])
-            else:
-                Author_Univ_Year[author_key].setdefault(univ_key,[])
-                Author_Univ_Year[author_key][univ_key].append(lines[i+2][3:-1])
-                
-for  author_key in multi_author_del:
-    multi_author=author_key.split(';')
-    univ_key=list(Author_Univ_Year[author_key].keys())[0]
-    multi_univ=str(univ_key).split(';')
-    year=Author_Univ_Year[author_key][univ_key]
-    for i,j in zip(multi_author,multi_univ):
-        if i in Author_Univ_Year:
-            if j in Author_Univ_Year[i]:
-                Author_Univ_Year[i][j].append(year)
-            else:
-        
-                Author_Univ_Year[i].setdefault(j,[])
-                Author_Univ_Year[i][j].append(year)
-        else:
-           
-            Author_Univ_Year.setdefault(i,{})
-            Author_Univ_Year[i].setdefault(j,[])
-            Author_Univ_Year[i][j].append(year)
-
-for  author_key in multi_author_del:
-        Author_Univ_Year.pop(author_key,None)
-    
-
-                
