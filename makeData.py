de2893d2121d3c9d66e5aa5d494c8ec04fb40c70
Bo Chen <bchen16@u.rochester.edu>
2017-11-25 20:34:53 -0500

    makedata

--- makeData.py
+++ makeData.py
@@ -1,9 +1,42 @@
 import sys
 
+print('Loading Aminer-Paper.txt')
 with open('Aminer-Paper.txt','r') as f:
 	lines = list(f.readlines())
+print('Complete!')
+print('Loading Aminer-Author.txt')
+with open('AMiner-Author.txt','r') as f:
+        Alines = list(f.readlines())
+print('Complete!')
 
+#Author dictionary raw data from Aminer-Paper.txt
+Author_RawUniversity = {}
+#Usable Author dictionary, with author name/affiliation-year information
 Author_Univ_Year={}
+
+#Loading author information from Aminer-Author.txt, omitting those without affiliation
+i = 0
+while(i < len(Alines)):
+    key = ''
+    affiliation  = ''
+    if  len(Alines[i]) > 6:
+        if Alines[i][1] == 'i' and Alines[i][2] == 'n' and Alines[i][3] == 'd' and Alines[i][4] == 'e' and Alines[i][5] == 'x':
+            key = Alines[i+1][3:]
+            affiliation = Alines[i+2][3:]
+            if len(affiliation)>8:
+                Author_RawUniversity[key] = affiliation
+            i = i + 9       
+        else:
+            i = i + 1
+    else:
+        i = i + 1
+    if i%1000000 == 0:
+        print(len(Author_RawUniversity))
+
+print('Total author loaded with affiliation(s):' + str(len(Author_RawUniversity)))
+
+
+#Loading author information from Aminer-Paper.txt, pairing with chronical information
 multi_author=[]
 multi_univ=[]
 multi_author_del=[]
@@ -46,6 +79,5 @@ for  author_key in multi_author_del:
 for  author_key in multi_author_del:
         Author_Univ_Year.pop(author_key,None)
     
-
 
                 
